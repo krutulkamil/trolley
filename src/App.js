df14a6ec70e74b7be3664cf6c8657d2ad6d2b4b1
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2021-12-30 23:44:18 +0100

    filtering & sorting products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, {useState} from 'react';
 import data from './data.json';
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 const App = () => {
     const [products, setProducts] = useState(data.products);
     const [size, setSize] = useState('');
     const [sort, setSort] = useState('');
+
+    const sortProducts = e => {
+        let currentSort = e.target.value;
+        setSort(currentSort);
+        setProducts(products.slice().sort((a, b) => currentSort === "lowest" ? a.price > b.price ? 1 : -1
+            : currentSort === "highest" ? a.price < b.price ? 1 : -1 : a._id < b._id ? 1 : -1))
+    };
+
+    const filterProducts = e => {
+        if (e.target.value === '') {
+            setSize(e.target.value);
+            setProducts(data.products);
+        } else {
+            setSize(e.target.value);
+            setProducts(data.products.filter(product => product.availableSizes.indexOf(e.target.value) >= 0));
+        }
+    };
 
     return (
         <div className="grid-container">
@@ -15,6 +33,13 @@ const App = () => {
             <main>
                 <div className="content">
                     <div className="main">
+                        <Filter
+                            count={products.length}
+                            size={size}
+                            sort={sort}
+                            filterProducts={filterProducts}
+                            sortProducts={sortProducts}
+                        />
                         <Products products={products}/>
                     </div>
                     <div className="sidebar">Cart Items</div>
