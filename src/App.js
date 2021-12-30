911cddda50beb6feee92df81e248f63350a14c84
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2021-12-30 20:47:26 +0100

    CRA & basic setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => (
+    <div className="grid-container">
+        <header>
+            <a href="/">TROLLEY</a>
+        </header>
+        <main>
+            Product List
+        </main>
+        <footer>
+            All right is reserved.
+        </footer>
+    </div>
+);
+
+export default App;
