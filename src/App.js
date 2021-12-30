567d1d5f88e009e3ef2b42b34811a2ce2d71d009
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2021-12-30 21:54:01 +0100

    create product component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import data from './data.json';
+import Products from "./components/Products";
 
-const App = () => (
-    <div className="grid-container">
-        <header>
-            <a href="/">TROLLEY</a>
-        </header>
-        <main>
-            Product List
-        </main>
-        <footer>
-            All right is reserved.
-        </footer>
-    </div>
-);
+const App = () => {
+    const [products, setProducts] = useState(data.products);
+    const [size, setSize] = useState('');
+    const [sort, setSort] = useState('');
+
+    return (
+        <div className="grid-container">
+            <header>
+                <a href="/">TROLLEY</a>
+            </header>
+            <main>
+                <div className="content">
+                    <div className="main">
+                        <Products products={products}/>
+                    </div>
+                    <div className="sidebar">Cart Items</div>
+                </div>
+            </main>
+            <footer>
+                All right is reserved.
+            </footer>
+        </div>
+    )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebfontLoader from '@dr-kobros/react-webfont-loader';
 import './index.css';
 import App from './App';
 
+const config = {
+    google: {
+        families: ['Montserrat:300,400,600,700'],
+    }
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <WebfontLoader config={config}>
+            <App/>
+        </WebfontLoader>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
